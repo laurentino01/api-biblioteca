10aabccb04cbad60bfb5819ab90e0cb6b2a69b97
Gabriel Laurentino <gabriellaurentino@hotmail.com>
2023-08-11 17:29:58 -0300

    teste de rota

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,15 @@
-import Express from "express";
+import Express, { Request, Response } from "express";
 import livrosRouter from "./livrosRoutes";
 
 export class Routes {
   private constructor() {}
 
   static routes(app: Express.Application) {
+    app.use("/", (req: Request, res: Response) => {
+      res.status(200).json({
+        message: "ok",
+      });
+    });
     app.use("/livros", livrosRouter);
     return;
   }
