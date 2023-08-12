4172e52850396b3654e1984e3c5381c95056db52
Gabriel Laurentino <gabriellaurentino@hotmail.com>
2023-08-11 21:23:58 -0300

    CONFIG CORS

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import * as dotenv from "dotenv";
 import { Routes } from "./routes/routes";
@@ -7,12 +7,12 @@ const app = express();
 
 dotenv.config();
 
-app.use(
-  cors({
-    origin: process.env.AUTH_DOMAIN,
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", process.env.AUTH_DOMAIN);
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  app.use(cors());
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
